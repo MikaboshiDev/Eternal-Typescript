f8a9cc39123fae4e4eb18259ff48dec79af3638d
MikaboshiDev <21011073@itsoeh.edu.mx>
2023-11-13 01:15:39 -0600

    Update Discord.js imports and refactor economy leaderboard command.

--- src/app/components/💴 economy/top.ts
+++ src/app/components/💴 economy/top.ts
@@ -1,6 +1,22 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelType, EmbedBuilder, Message } from 'discord.js';
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ChannelType,
+  EmbedBuilder,
+  GuildMember,
+  Message,
+} from 'discord.js';
 import emojis from '../../../../config/emojis.json';
 import model from '../../../models/servers/economy';
+import { pagination } from '../../../functions/tools/pagination';
+
+const medallas: { [key: number]: string | undefined } = {
+  1: '🥇',
+  2: '🥈',
+  3: '🥉',
+};
+
 module.exports = {
   name: 'top',
   description: 'Shows the richest users',
@@ -9,61 +25,26 @@ module.exports = {
   premium: false,
   cooldown: 5000,
   async execute(client: any, message: Message, args: string[], prefix: any) {
-    const data = await model.find();
-    if (!message.guild) return;
+    const total = await model.find();
+    await message.guild?.members.fetch();
+    const ordenado = total
+      .filter((member: any) => message.guild?.members.cache.get(member.userID))
+      .sort((a, b) => Number(b.money + b.bank - (a.money + a.bank)));
 
-    await message.guild.members.fetch();
-    const members = message.guild.members.cache.filter((member) => !member.user.bot);
-    const users = data.filter((user) => members.has(user.userID));
-    const sorted = users.sort((a, b) => b.money - a.money);
+    const texto = ordenado.map((miembro, index) => {
+      const guildMember = message.guild?.members.cache.get(miembro.userID);
 
-    const embed = new EmbedBuilder()
-      .setTitle(`${emojis.money} ${message.guild.name}\'s Leaderboard`)
-      .setColor('Random')
-      .setTimestamp();
+      if (guildMember) {
+        const medalla = medallas[index + 1];
 
-    const button = new ActionRowBuilder().addComponents(
-      new ButtonBuilder().setCustomId('next').setLabel('Next').setStyle(ButtonStyle.Primary),
-      new ButtonBuilder().setCustomId('previous').setLabel('Previous').setStyle(ButtonStyle.Primary),
-      new ButtonBuilder().setCustomId('first').setLabel('First').setStyle(ButtonStyle.Primary)
-    );
-
-    embed.setDescription(
-      sorted
-        .slice(0, 10)
-        .map((user, index) => `${index + 1}. <@${user.userID}> - ${user.money.toLocaleString()} ${emojis.coin}`)
-        .join('\n')
-    );
-
-    const msg = await message.reply({ embeds: [embed], components: [button as any] });
-    const filter = (i: any) => i.user.id === message.author.id;
-    const collector = msg.createMessageComponentCollector({ filter, time: 60000 });
-
-    let page = 0;
-    collector.on('collect', async (i: any) => {
-      if (i.customId === 'next') {
-        page++;
-        if (page > Math.ceil(sorted.length / 10) - 1) page = 0;
-      } else if (i.customId === 'previous') {
-        page--;
-        if (page < 0) page = Math.ceil(sorted.length / 10) - 1;
-      } else if (i.customId === 'first') {
-        page = 0;
+        return `${medalla ?? ''} \`${index + 1}\` - <@${miembro.userID}> *\`${guildMember.user.tag}\`*\n**Money:** \`${
+          miembro.money
+        }\` 🪙\n**Bank:** \`${miembro.bank}\` 🏦\n\n`;
+      } else {
+        return '';
       }
-
-      const embed = new EmbedBuilder()
-        .setTitle(`${emojis.money} ${message.guild?.name}\'s Leaderboard`)
-        .setColor('Random')
-        .setTimestamp();
-
-      embed.setDescription(
-        sorted
-          .slice(page * 10, page * 10 + 10)
-          .map((user, index) => `${index + 1}. <@${user.userID}> - ${user.money.toLocaleString()} ${emojis.coin}`)
-          .join('\n')
-      );
-
-      await i.update({ embeds: [embed] });
     });
+
+    pagination(client, message, texto, '💸 ECONOMY LEADERBOARD 💸');
   },
 };
---
+++ src/functions/tools/pagination.ts
@@ -0,0 +1,143 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, MessageComponentInteraction } from 'discord.js';
+import emojis from '../../../config/emojis.json';
+export async function pagination(
+  client: any,
+  message: any,
+  texto: string[],
+  titulo = 'Paginación',
+  elementos_por_pagina = 5
+) {
+  const embeds: any[] = [];
+  let elementos_por_pagina_actual = elementos_por_pagina;
+
+  for (let i = 0; i < texto.length; i += elementos_por_pagina) {
+    const desc = texto.slice(i, elementos_por_pagina_actual);
+    elementos_por_pagina_actual += elementos_por_pagina;
+
+    const embed = new EmbedBuilder()
+      .setTitle(titulo.toString())
+      .setDescription(desc.join(' '))
+      .setColor(client.color)
+      .setThumbnail(message.guild.iconURL({ forceStatic: true }));
+    embeds.push(embed);
+  }
+
+  let paginaActual = 0;
+
+  if (embeds.length === 1) return message.reply({ embeds: [embeds[0]] }).catch(() => {});
+
+  const boton_atras = new ButtonBuilder().setStyle(ButtonStyle.Success).setCustomId('Atrás').setLabel('Back');
+  const boton_inicio = new ButtonBuilder().setStyle(ButtonStyle.Danger).setCustomId('Inicio').setLabel('Home');
+  const boton_avanzar = new ButtonBuilder().setStyle(ButtonStyle.Success).setCustomId('Avanzar').setLabel('Forward');
+
+  const embedpaginas = await message.reply({
+    content: `**Click on the __Buttons__ to change pages according to your interest**`,
+    embeds: [embeds[0]],
+    components: [new ActionRowBuilder().addComponents([boton_atras, boton_inicio, boton_avanzar])],
+  });
+
+  const collector = embedpaginas.createMessageComponentCollector({
+    filter: (i: MessageComponentInteraction) =>
+      i.isButton() && i.user && i.user.id == message.author.id && i.message.author.id == client.user.id,
+    time: 180e3,
+  });
+
+  collector.on('collect', async (b: any) => {
+    if (b.user.id !== message.author.id)
+      return b.reply({
+        content: [
+          `${emojis.error} **You are not allowed to use this button because you did not execute the command**`,
+          `**Only** <@${message.author.id}> **can use it**`,
+        ].join('\n'),
+      });
+
+    switch (b.customId) {
+      case 'Atrás':
+        {
+          collector.resetTimer();
+
+          if (paginaActual !== 0) {
+            paginaActual -= 1;
+
+            await embedpaginas
+              .edit({
+                embeds: [embeds[paginaActual]],
+                components: [embedpaginas.components[0]],
+              })
+              .catch(() => {});
+            await b.deferUpdate();
+          } else {
+            paginaActual = embeds.length - 1;
+
+            await embedpaginas
+              .edit({
+                embeds: [embeds[paginaActual]],
+                components: [embedpaginas.components[0]],
+              })
+              .catch(() => {});
+            await b.deferUpdate();
+          }
+        }
+        break;
+
+      case 'Inicio':
+        {
+          collector.resetTimer();
+
+          paginaActual = 0;
+          await embedpaginas
+            .edit({
+              embeds: [embeds[paginaActual]],
+              components: [embedpaginas.components[0]],
+            })
+            .catch(() => {});
+          await b.deferUpdate();
+        }
+        break;
+
+      case 'Avanzar':
+        {
+          collector.resetTimer();
+
+          if (paginaActual < embeds.length - 1) {
+            paginaActual++;
+
+            await embedpaginas
+              .edit({
+                embeds: [embeds[paginaActual]],
+                components: [embedpaginas.components[0]],
+              })
+              .catch(() => {});
+            await b.deferUpdate();
+          } else {
+            paginaActual = 0;
+
+            await embedpaginas
+              .edit({
+                embeds: [embeds[paginaActual]],
+                components: [embedpaginas.components[0]],
+              })
+              .catch(() => {});
+            await b.deferUpdate();
+          }
+        }
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  collector.on('end', () => {
+    embedpaginas.components[0].components.map((boton: { setDisabled: (arg0: boolean) => any }) =>
+      boton.setDisabled(true)
+    );
+    embedpaginas
+      .edit({
+        content: `${emojis.error} Oops your time has expired come back later and execute the command again`,
+        embeds: [embeds[paginaActual]],
+        components: [embedpaginas.components[0]],
+      })
+      .catch(() => {});
+  });
+}
